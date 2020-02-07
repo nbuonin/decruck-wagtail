83eb04fdbc89d47d822d721c83961a24be4557c8
Nick Buonincontri <nbuonin@gmail.com>
2020-02-06 22:26:07 -0500

    Move tests location

---
+++ decruck/main/tests/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class CompositionModelTest(TestCase):
+    # write some code to bootstrap a homepage, composition listing page
+    # and a composition
+    # Think about factories and a mixin
+    def test_test(self):
+        self.assertTrue(True)
