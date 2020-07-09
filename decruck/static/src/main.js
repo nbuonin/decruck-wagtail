5d5c77e30d5b50d60b05d7109fae008631bc71c6
Nick Buonincontri <nbuonin@gmail.com>
2020-07-09 14:13:25 -0400

    Refine navigation implementation
    This commit refactors the nav to make use of Bootstraps standard nav. It
    also changes the visibility of the language picker, and smooths out some
    other rough edges.

--- decruck/static/src/main.js
+++ decruck/static/src/main.js
@@ -3,19 +3,6 @@ import 'bootstrap';
 import './scss/main.scss';
 import "@fortawesome/fontawesome-free/js/all";
 
-// Mobile Nav
-$('#navbar-toggle').on('click', function(el){
-    $('.nav').first().toggleClass('mobile-nav-active');
-})
-
-$('.composition-search__fieldset-checkbox-label').on('click', function(el){
-    $('#composition-search__form').submit();
-});
-
-$('.composition-search__fieldset-sort-label').on('click', function(el){
-    $('#composition-search__form').submit();
-});
-
 // Checkout Form
 $('#shopping-cart__proxy-button').on('click', function(el){
     $('#shopping-cart__pp-form-container form').submit();
