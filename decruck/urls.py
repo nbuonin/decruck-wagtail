9df689ed8ca316862e0761b79288dfdc144a1d51
Nick Buonincontri <nbuonin@gmail.com>
2021-01-03 15:59:16 -0500

    Install debug toolbar

--- decruck/urls.py
+++ decruck/urls.py
@@ -1,3 +1,4 @@
+import debug_toolbar
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
@@ -33,3 +34,6 @@ if settings.DEBUG:
     # Serve static and media files from development server
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    # Setup Debug toolbar
+    urlpatterns = [url('__debug__/', include(debug_toolbar.urls))] + urlpatterns
